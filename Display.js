29ba912dac10a3f2e015fc74076646afbb389f9d
mohammed s. yaghi <yaghi.moh@gmail.com>
2020-03-26 16:59:16 +0200

    Add files via upload

---
+++ Display.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+export default class Display extends React.Component{
+    render() {
+
+        const mystyle={
+            color:"red",
+            backgroundColor:"black",
+            padding:"10px"
+
+        }
+
+        return (
+            <div style={mystyle}>Welcome!!!!!</div>
+        )
+    }
+
+}
